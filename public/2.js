2a2aa156e30a3e39b0b6d123f251a3679fd29b92
greemv <hvitl@mail.ru>
2021-04-26 17:57:42 +0600

    123

---
+++ public/2.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[2],{
+
+/***/ "./resources/js/Pages/About.vue":
+/*!**************************************!*\
+  !*** ./resources/js/Pages/About.vue ***!
+  \**************************************/
+/*! exports provided: default */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/vue-loader/lib/index.js):\nError: ENOENT: no such file or directory, open 'F:\\progs\\OSPanel\\domains\\gallery-app\\resources\\js\\Pages\\About.vue'");
+
+/***/ })
+
+}]);
---
+++ resources/js/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <router-view></router-view>
+        <h2>default</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App"
+};
+</script>
+
+<style scoped>
+
+</style>
