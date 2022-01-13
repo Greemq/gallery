f19a404c99c108344c4e5d9a41ceab3fdc5bf6ea
greemv <hvitl@mail.ru>
2022-01-13 21:55:45 +0600

    fix

--- resources/js/Router/routes.js
+++ resources/js/Router/routes.js
@@ -1,9 +1,9 @@
 const routes =[
-    // {
-    //     path:'/',
-    //     component:()=>import('../Pages/Gallery/List.vue'),
-    //     name:'list'
-    // },
+    {
+        path:'/list',
+        component:()=>import('../Pages/Gallery/List.vue'),
+        name:'list'
+    },
     {
         path:'/home',
         component:()=>import('../Pages/Home.vue'),
